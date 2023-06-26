e6540775270e5c602a999fce77df39a454be6ada
Michel Pomerantzeff <micheldauniao@hotmail.com>
2023-06-26 08:28:34 +0100

    fix:: adjusted items pricing

--- src/data.js
+++ src/data.js
@@ -3,30 +3,31 @@
 export const dishes = {
     
     beef: [
-        { id: "1.1", title: "Steak on mustard", price: "18.50", info: "Tapioca flour and cabbage", image: "./images/menu/dishes/beef/mustard-mignon.png", weight: "140g", units: "" },
-        { id: "1.2", title: "Meat balls with nuts", price: "12.90", info: "Roasted potato slices with garlic and parsley + Brocolli", image: "./images/menu/dishes/beef/quibe-nut.png", weight: "210g", units: "" }
+        { id: "1.1", title: "Steak on mustard", price: "15.80", info: "Tapioca flour and cabbage", image: "./images/menu/dishes/beef/mustard-mignon.png", weight: "140g", units: "" },
+        { id: "1.2", title: "Meat balls with nuts", price: "9.40", info: "Mediterranean lentil", image: "./images/menu/dishes/beef/quibe-nut.png", weight: "210g", units: "" },
+        { id: "1.3", title: "Meat balls with nuts", price: "10.60", info: "Roasted potato slices with garlic and parsley + Brocolli", image: "./images/menu/dishes/beef/meat-balls-brocolli.png", weight: "310g", units: "" }
     ],
     chicken: [
-        { id: "2.1", title: "Creamy chicken", info: "Whole meal rice and beans", price: "16.50", image: "./images/menu/dishes/chicken/with-rice-beans.png", weight: "310g", units: "" },
-        { id: "2.2", title: "Chicken with peanut and lemon pepper", info: "Low carb mashed pumpkin", price: "17.00", image:"./images/menu/dishes/chicken/with-peanut.png", weight: "220g", units: "" },
-        { id: "2.3", title: "Creamy chicken", info: "Whole meal rice with almonds and brocolli + pumpkin slices and coconut oil", price: "19.50", image:"./images/menu/dishes/chicken/pumpkin-chicken.png", weight: "330g", units: "" },
-        { id: "2.4", title: "Creamy chicken", info: "Cauliflower rice with carots and nuts ", price: "20.90", image:"./images/menu/dishes/chicken/creamy-chicken.png", weight: "250g", units: "" },
-        { id: "2.5", title: "Chicken with lemon and mint", info: "Jasmin rice with onion rings and nuts", price: "16.50", image:"./images/menu/dishes/chicken/with-lemon.png", weight: "210g", units: "" }
+        { id: "2.1", title: "Creamy chicken", info: "Whole meal rice and beans", price: "10.50", image: "./images/menu/dishes/chicken/with-rice-beans.png", weight: "310g", units: "" },
+        { id: "2.2", title: "Chicken with peanut and lemon pepper", info: "Low carb mashed pumpkin", price: "13.90", image:"./images/menu/dishes/chicken/with-peanut.png", weight: "220g", units: "" },
+        { id: "2.3", title: "Creamy chicken", info: "Whole meal rice with almonds and brocolli + pumpkin slices and coconut oil", price: "12.50", image:"./images/menu/dishes/chicken/pumpkin-chicken.png", weight: "330g", units: "" },
+        { id: "2.4", title: "Creamy chicken", info: "Cauliflower rice with carots and nuts ", price: "10.90", image:"./images/menu/dishes/chicken/creamy-chicken.png", weight: "250g", units: "" },
+        { id: "2.5", title: "Chicken with lemon and mint", info: "Jasmin rice with onion rings and nuts", price: "11.50", image:"./images/menu/dishes/chicken/with-lemon.png", weight: "210g", units: "" }
     ],
     fish: [
-        { id: "3.1", title: "Saint Peter with herbs and almond", info: "Black rice", price: "22.00", image: "./images/menu/dishes/fish/saint-peter-almond.png", weight: "210g", units: "" },
-        { id: "3.2", title: "Saint Peter with shrimp sauce", info: "Wholemeal rice + Cabbage", price: "26.50", image: "./images/menu/dishes/fish/saint-peter-shrimp.png", weight: "280g", units: "" }
+        { id: "3.1", title: "Saint Peter with herbs and almond", info: "Black rice", price: "14.00", image: "./images/menu/dishes/fish/saint-peter-almond.png", weight: "210g", units: "" },
+        { id: "3.2", title: "Saint Peter with shrimp sauce", info: "Wholemeal rice + Cabbage", price: "15.50", image: "./images/menu/dishes/fish/saint-peter-shrimp.png", weight: "280g", units: "" }
     ],
     pasta: [
-        { id: "4.1", title: "Three cheese Rondelli", info: "Tomato sauce", price: "19,90", image: "./images/menu/dishes/pasta/rondelli.png", weight: "260g", units: "" },
-        { id: "4.2", title: "Raviolli with gorgonzola, pear and hazelnut", info: "Sour cream and parmesian", price: "18.50", image: "./images/menu/dishes/pasta/ravioli.png", weight: "230g", units: "" },
-        { id: "4.3", title: "Canelloni cheese with spinach", info: "Tomato sauce", price: "20.00", image: "./images/menu/dishes/pasta/pasta-spinach.png", weight: "260g", units: "" }
+        { id: "4.1", title: "Three cheese Rondelli", info: "Tomato sauce", price: "15.90", image: "./images/menu/dishes/pasta/rondelli.png", weight: "260g", units: "" },
+        { id: "4.2", title: "Raviolli with gorgonzola, pear and hazelnut", info: "Sour cream and parmesian", price: "13.50", image: "./images/menu/dishes/pasta/ravioli.png", weight: "230g", units: "" },
+        { id: "4.3", title: "Canelloni cheese with spinach", info: "Tomato sauce", price: "15.00", image: "./images/menu/dishes/pasta/pasta-spinach.png", weight: "260g", units: "" }
     ],
     vegetarian: [
-        { id: "5.1", title: "Falafel", info: "Mediterranean lentil", price: "16.50", image: "./images/menu/dishes/vegetarian/falafel.png", weight: "220g", units: "" },
-        { id: "5.2", title: "Mushroom burger", info: "couscous with cashew", price: "17.00", image: "./images/menu/dishes/vegetarian/mushroom-burger.png", weight: "210g", units: "" },
-        { id: "5.3", title: "Falafel", info: "Jamin rice with onion ring + Mashed pumpkin", price: "20.90", image: "./images/menu/dishes/vegetarian/pumpkin.png", weight: "320g", units: "" },
-        { id: "5.4", title: "Wholemeal rice", info: "Pumpkin slices and chickpea", price: "17.90", image: "./images/menu/dishes/vegetarian/wholemeal-rice.png", weight: "210g", units: "" }
+        { id: "5.1", title: "Falafel", info: "Mediterranean lentil", price: "13.50", image: "./images/menu/dishes/vegetarian/falafel.png", weight: "220g", units: "" },
+        { id: "5.2", title: "Mushroom burger", info: "couscous with cashew", price: "14.00", image: "./images/menu/dishes/vegetarian/mushroom-burger.png", weight: "210g", units: "" },
+        { id: "5.3", title: "Falafel", info: "Jamin rice with onion ring + Mashed pumpkin", price: "16.90", image: "./images/menu/dishes/vegetarian/pumpkin.png", weight: "320g", units: "" },
+        { id: "5.4", title: "Wholemeal rice", info: "Pumpkin slices and chickpea", price: "15.90", image: "./images/menu/dishes/vegetarian/wholemeal-rice.png", weight: "210g", units: "" }
     ]
 }
 
@@ -34,28 +35,28 @@ export const dishes = {
 export const healthy = {
     
     day_one: [
-        { id: "6.1", title: "Brocolli rice", price: "5.90", image: "./images/menu/healthy/day-1/brocolli-rice.png", units: "" },
+        { id: "6.1", title: "Brocolli rice", price: "4.50", image: "./images/menu/healthy/day-1/brocolli-rice.png", units: "" },
         { id: "6.2", title: "Palm-heart", price: "6.00", image: "./images/menu/healthy/day-1/palmito.png", units: "" },
         { id: "6.3", title: "Pumpkin roll", price: "5.00", image: "./images/menu/healthy/day-1/pumpkin-roll.png", units: "" }
     ],
     day_two: [
-        { id: "7.1", title: "Shredded cashew", price: "5.90", image: "./images/menu/healthy/day-2/mashed-cashew.png", units: "" },
-        { id: "7.2", title: "Mashed pumpkin", price: "6.00", image: "./images/menu/healthy/day-2/mashed-pumpkim.png", units: "" },
-        { id: "7.3", title: "Mignon slices", price: "16.00", image: "./images/menu/healthy/day-2/mignon-slices.png", units: "" }
+        { id: "7.1", title: "Shredded cashew", price: "4.90", image: "./images/menu/healthy/day-2/mashed-cashew.png", units: "" },
+        { id: "7.2", title: "Mashed pumpkin", price: "5.00", image: "./images/menu/healthy/day-2/mashed-pumpkim.png", units: "" },
+        { id: "7.3", title: "Mignon slices", price: "10.00", image: "./images/menu/healthy/day-2/mignon-slices.png", units: "" }
     ],
     day_three: [
-        { id: "8.1", title: "Crusted chicken", price: "9.90", image: "./images/menu/healthy/day-3/crusted-chickeb.png", units: "" },
+        { id: "8.1", title: "Crusted chicken", price: "6.90", image: "./images/menu/healthy/day-3/crusted-chickeb.png", units: "" },
         { id: "8.2", title: "Roasted carrot", price: "4.00", image: "./images/menu/healthy/day-3/roasted-carrot.png", units: "" },
         { id: "8.3", title: "Sweet potato", price: "4.00", image: "./images/menu/healthy/day-3/sweet-potato.png", units: "" }
     ],
     day_four: [
-        { id: "9.1", title: "Jasmin rice", price: "4.00", image: "./images/menu/healthy/day-4/jasmin-rice.png", units: "" },
-        { id: "9.2", title: "Pumpkin slices", price: "4.50", image: "./images/menu/healthy/day-4/pumpkim-slices.png", units: "" },
-        { id: "9.3", title: "Saint Peter with lime", price: "12.00", image: "./images/menu/healthy/day-4/saint-peter.png", units: "" }
+        { id: "9.1", title: "Jasmin rice", price: "4.50", image: "./images/menu/healthy/day-4/jasmin-rice.png", units: "" },
+        { id: "9.2", title: "Pumpkin slices", price: "5.00", image: "./images/menu/healthy/day-4/pumpkim-slices.png", units: "" },
+        { id: "9.3", title: "Saint Peter with lime", price: "10.00", image: "./images/menu/healthy/day-4/saint-peter.png", units: "" }
     ],
     day_five: [
-        { id: "10.1", title: "Brocolli rice", price: "5.90", image: "./images/menu/healthy/day-5/brocolli-rice.png", units: "" },
-        { id: "10.2", title: "Crusted chicken", price: "9.90", image: "./images/menu/healthy/day-5/crusted-chicken.png", units: "" },
+        { id: "10.1", title: "Brocolli rice", price: "4.50", image: "./images/menu/healthy/day-5/brocolli-rice.png", units: "" },
+        { id: "10.2", title: "Crusted chicken", price: "6.90", image: "./images/menu/healthy/day-5/crusted-chicken.png", units: "" },
         { id: "10.3", title: "Roasted corgette", price: "4.00", image: "./images/menu/healthy/day-5/roasted-corgette.png", units: "" }
     ]
 }
