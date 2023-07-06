fd3cf910203a04953c871f32424d0e2397bc4859
Michel Pomerantzeff <micheldauniao@hotmail.com>
2023-07-06 01:13:11 +0100

    feat:: manage cartItems in local storage to persist data on page refresh

--- src/redux/features/cartSlice.js
+++ src/redux/features/cartSlice.js
@@ -1,29 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
 export const cartSlice = createSlice({
   name: "cart",
-  initialState: [],
-  totalItems: {},
+  initialState,
   reducers: {
     add: (state, action) => {
-      const existingItem = state.find((i) => action.payload.id == i.id);
+      const existingItem = state.find((item) => action.payload.id == item.id);
       if (existingItem) {
-        existingItem.units++;
-        state = { ...state, existingItem };
+        state.map((item) => {
+          if (item.id == action.payload.id) {
+            item.units++;
+          }
+        });
+        localStorage.setItem("cartItems", JSON.stringify(state));
       } else {
-        const xxx = (action.payload.units = 1);
-        state.push({ ...action.payload, xxx });
+        action.payload.units = 1;
+        state.push({ ...action.payload });
+        localStorage.setItem("cartItems", JSON.stringify(state));
       }
     },
 
     remove: (state, action) => {
       const existingItem = state.find((i) => action.payload.id == i.id);
 
-      if (existingItem.units > 1) {
-        existingItem.units--;
-        state = { ...state, existingItem };
+      if (existingItem?.units > 1) {
+        state.map((item) => {
+          if (item.id == action.payload.id) {
+            item.units--;
+          }
+        });
+        localStorage.setItem("cartItems", JSON.stringify(state));
       } else {
-        return state.filter((item) => item.id !== action.payload.id);
+        const deleted = state.filter((item) => item.id !== action.payload.id);
+        localStorage.setItem("cartItems", JSON.stringify(deleted));
+        return deleted;
       }
     },
 
